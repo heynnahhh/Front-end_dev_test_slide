7604a6612055fa48808210383ba12b8e7713927f
Hannah Liao <hannahpatricia.liao@gmail.com>
2024-08-08 03:53:10 +0800

    Refactored Test Slide for memory usage concerns

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,11 @@
 var currentIndex = 0;
+var pointer = 0;
 var tl = null;
 var data = [];
 
 $(document).ready(function() {
 	parseData();
-    insertItemData();
+    insertInitialItemData();
 });  
 
 $(window).load(function () {
@@ -15,12 +16,38 @@ function parseData() {
 	data = JSON.parse($('.DataHolder').text());
 }
 
-function insertItemData(){
+/**
+ * Sets initial div.Item that contains the first 2 images in the data array
+ * @returns nothing
+ */
+function insertInitialItemData(){
+	$('.DisplayContainer').css('width', 2 * 100 + '%');
 
-	$('.DisplayContainer').css('width', data.length * 100 + '%');
-	
-	for (var i = 0; i < data.length; i++) {
-		$('.DisplayContainer').append('<div class="Item" style="background-image: url(' + data[i].imgPath + '); width: ' + (1 / data.length * 100) + '%;"></div>');
+	for (var i = pointer; i <= pointer + 1 && i < data.length; i++) {
+		$('.DisplayContainer').append('<div class="Item" id=' + i + ' style="background-image: url(' + data[i].imgPath + '); width: ' + (1 / 2 * 100) + '%;"></div>');
+	}
+
+	pointer+=2;
+}
+
+/**
+ * Inserts the next image in the data array as div.Item
+ * @returns nothing
+ */
+function insertNextItemData(){
+	if(pointer < data.length){
+		$('.DisplayContainer').append('<div class="Item" id=' + pointer + ' style="background-image: url(' + data[pointer].imgPath + '); width: ' + (1 / 2 * 100) + '%;"></div>');
+		pointer++;
+	}
+}
+
+/**
+ * Removes the first div.Item inside div.DisplayContainer
+ * @returns nothing
+ */
+function removeFirstItemData(){
+	if (currentIndex < data.length - 1) {
+		$('div.Item:first').remove();
 	}
 }
 
@@ -32,14 +59,19 @@ function animateSlide() {
 
 function animateItem() {
 	if (currentIndex < data.length - 1) {
-		
-		tl.to(
-			'.DisplayContainer', 			// move .DisplayContainer
-			1, 								// moving time is 1 second
-			{x: "-=" + $(window).width()} 	// move .DisplayContainer to the left by a distance of screen width
-		);
+		tl.fromTo(
+			'.DisplayContainer', 						// move .DisplayContainer
+			1, 											// moving time is 1 second
+			{x: 0},										// starting position of animation
+			{x: "-=" + $(window).width()} 				// move .DisplayContainer to the left by a distance of screen width
+		)
+		.add(insertNextItemData);						//adds callback function to the timeline
 
 		currentIndex++;
-		setTimeout(animateItem, 3000);
+		setTimeout(function(){
+			removeFirstItemData();						// remove transitioned first div.Item
+			animateItem();
+		}
+		, 3000);
 	}
 }
